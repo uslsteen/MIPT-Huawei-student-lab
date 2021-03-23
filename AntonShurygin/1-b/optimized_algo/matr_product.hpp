cd6d6f57c3ef5edd9fb5f794b06231bdcd8e525c
uslsteen <shurygin0171@gmail.com>
2021-03-23 18:29:08 +0300

    something doesn't work

--- AntonShurygin/1-b/optimized_algo/matr_product.hpp
+++ AntonShurygin/1-b/optimized_algo/matr_product.hpp
@@ -16,17 +16,36 @@ namespace Linear_space
 
         assert(lhs_cols == rhs_rows);
 
-
         for (uint i = 0; i < lhs_rows; ++i)
+        {
+            float* res = result[i];
 
-            for (uint j = 0; j < rhs_cols; ++j)
+            for (uint j = 0; j < rhs_cols/8; ++j)
+               _mm256_storeu_ps(res + j*8 + 0, _mm256_setzero_ps());
 
-                for (uint k = 0; k < rhs_rows; ++k)
-                    result[i][j] += lhs[i][k] * rhs[k][j];
+            for (uint k = 0; k < lhs_cols; ++k)
+            {
+                const float* rhs_row = rhs[k];
+                __m256 lhs_elem = _mm256_set1_ps(lhs[i][k]);
+
+                float lhs_elem_ = lhs[i][k];
 
 
+                for (uint j = 0; j < rhs_cols/16; j++)
+                {
+                    _mm256_storeu_ps(res + j*8 + 0, _mm256_fmadd_ps(lhs_elem, _mm256_loadu_ps(rhs_row + j + 0), _mm256_loadu_ps(res + j + 0)));
+                    _mm256_storeu_ps(res + j*8 + 8, _mm256_fmadd_ps(lhs_elem, _mm256_loadu_ps(rhs_row + j + 8), _mm256_loadu_ps(res + j + 8)));
+                }
+
+
+                for (uint k = (rhs_cols - rhs_cols % 16) ; k < rhs_cols ; ++k)
+                    res[k] += rhs_row[k] * lhs_elem_;
+            }
+        }
     }
+
+
 
 }
 
-#endif __MATR_PRODUCT_HPP__   
+#endif __MATR_PRODUCT_HPP__
