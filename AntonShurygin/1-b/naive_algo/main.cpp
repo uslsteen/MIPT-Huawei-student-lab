3dabcfca7edcb25fc54633026619d19fc14d63c1
uslsteen <shurygin0171@gmail.com>
2021-04-08 16:25:27 +0300

    fix include

--- AntonShurygin/1-b/naive_algo/main.cpp
+++ AntonShurygin/1-b/naive_algo/main.cpp
@@ -1,4 +1,5 @@
-#include "matr_product.hpp"
+//#include "matr_product.hpp"
+#include "../optimized_algo/matr_product.hpp"
 #include "my_time.hpp"
 
 using namespace Linear_space;
@@ -10,9 +11,17 @@ using namespace Mul_optim;
 template <typename Data>
 void Buffer_init(Data& buffer, uint& num_of_rws, uint& num_of_clmns)
 {
+    #if  __INPUT__
+    std::cout << "Input size of you matrix.\nRows X Cols\n";
+    #endif
+
     std::cin >> num_of_rws >> num_of_clmns;
 
     buffer.reserve(num_of_rws * num_of_clmns);
+
+    #if __INPUT__
+    std::cout << "Input data for matrix in buffer:\n";
+    #endif
 
     for (size_t i = 0; i < num_of_rws*num_of_clmns; ++i)
         std::cin >> buffer[i];
@@ -23,21 +32,30 @@ void Mul_testing(func_info& func_struct, Mul_optim::Matr_int& naive_mul, Mul_opt
                                                                          Mul_optim::Matr_int& rhs, 
                                                                          Mul_optim::Matr_int result)
 {
+    #if __TIMER__
     Time::Timer mul_time;
+    #endif
+
     func_struct.func(lhs, rhs, result);
     
+    #if __TIMER__
     std::cout << std::endl << func_struct.func_name << " time:" << mul_time.elapsed() << " millisecs\n";
+    #endif
+
+    #if __MATR_OUT__
 
     std::cout << "res matrix:\n";
-
-
     OUT << result << "              <================= result\n";
 
+    #endif
+
+    #if __CORRECT__
     if (naive_mul == result)
         std::cout << "Mul is right!\n";
 
     else
         std::cout << "Something went wrong!\n";
+    #endif
 }
 
 
@@ -56,14 +74,26 @@ int main()
 
     Mul_optim::Matr_int naive_mul_{num_of_rws1, num_of_clmns2, 0};
 
+
+    std::cout << "Program have started! Please, wait!\n";
+    
+    #if __TIMER__
     Time::Timer naive_mul_time;
-    naive_mul(lhs, rhs, naive_mul_);
-    std::cout << std::endl << "Naive time:" << naive_mul_time.elapsed() << " millisecs\n";
+    #endif
+
+    naive_mul(lhs, rhs
+    , naive_mul_);
+
+    #if __TIMER__
+    std::cout << std::endl << "naive_mul time:" << naive_mul_time.elapsed() << " millisecs\n";
+    #endif
+
+    #if __MATR_OUT__
 
     std::cout << "res matrix:\n";
-
-
     OUT << naive_mul_ << "              <================= result\n";
+
+    #endif
 
 
     for (auto&& func: Mul_optim::functions)
