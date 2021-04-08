71f3d471943e6568dd7ed54135bc36ce01d0f095
uslsteen <shurygin0171@gmail.com>
2021-04-08 11:58:23 +0300

    some fixes

--- AntonShurygin/1-b/naive_algo/main.cpp
+++ AntonShurygin/1-b/naive_algo/main.cpp
@@ -2,6 +2,7 @@
 #include "my_time.hpp"
 
 using namespace Linear_space;
+using namespace Mul_optim;
 
 #define OUT std::cout
 
@@ -17,109 +18,58 @@ void Buffer_init(Data& buffer, uint& num_of_rws, uint& num_of_clmns)
         std::cin >> buffer[i];
 }
 
+
+void Mul_testing(func_info& func_struct, Mul_optim::Matr_int& naive_mul, Mul_optim::Matr_int& lhs, 
+                                                                         Mul_optim::Matr_int& rhs, 
+                                                                         Mul_optim::Matr_int result)
+{
+    Time::Timer mul_time;
+    func_struct.func(lhs, rhs, result);
+    
+    std::cout << std::endl << func_struct.func_name << " time:" << mul_time.elapsed() << " millisecs\n";
+
+    std::cout << "res matrix:\n";
+
+
+    OUT << result << "              <================= result\n";
+
+    if (naive_mul == result)
+        std::cout << "Mul is right!\n";
+
+    else
+        std::cout << "Something went wrong!\n";
+}
+
+
 int main()
 {
-    uint num_of_rws1 = 0, num_of_clmns1 = 0, num_of_rws2 = 0, num_of_clmns2 = 9;
-    std::vector<float> buffer1, buffer2;
+    uint num_of_rws1 = 0, num_of_clmns1 = 0, num_of_rws2 = 0, num_of_clmns2 = 0;
+    std::vector<int> buffer_lhs, buffer_rhs;
 
+    Buffer_init(buffer_lhs, num_of_rws1, num_of_clmns1);
+    Buffer_init(buffer_rhs, num_of_rws2, num_of_clmns2);
 
-    Buffer_init(buffer1, num_of_rws1, num_of_clmns1);
-    Buffer_init(buffer2, num_of_rws2, num_of_clmns2);
+    Mul_optim::Matr_int lhs{num_of_rws1, num_of_clmns1, buffer_lhs};
+    Mul_optim::Matr_int rhs{num_of_rws2, num_of_clmns2, buffer_rhs};
 
-    Linear_space::Matrix<float> mtr1{num_of_rws1, num_of_clmns1, buffer1};
-    Linear_space::Matrix<float> mtr2{num_of_rws2, num_of_clmns2, buffer2};
+    Mul_optim::Matr_int result{num_of_rws1, num_of_clmns2, 0};
 
-
-    Linear_space::Matrix<float> naive_mul_{num_of_rws1, num_of_clmns2, 0};
-    Linear_space::Matrix<float> fast_mul{num_of_rws1, num_of_clmns2, 0};
-    Linear_space::Matrix<float> unroll_mul_2x{num_of_rws1, num_of_clmns2, 0};
-    Linear_space::Matrix<float> unroll_mul_4x{num_of_rws1, num_of_clmns2, 0};
-    Linear_space::Matrix<float> unroll_mul_8x{num_of_rws1, num_of_clmns2, 0};
-
-
+    Mul_optim::Matr_int naive_mul_{num_of_rws1, num_of_clmns2, 0};
 
     Time::Timer naive_mul_time;
-    naive_mul(mtr1, mtr2, naive_mul_);
+    naive_mul(lhs, rhs, naive_mul_);
     std::cout << std::endl << "Naive time:" << naive_mul_time.elapsed() << " millisecs\n";
 
     std::cout << "res matrix:\n";
 
 
-    //OUT << naive_mul_ << "              <================= result\n";
-
-    /* <=====================================================================> */
-
-    Time::Timer unroll_mul_2x_time{};
-
-    mul_unroll_2x(mtr1, mtr2, unroll_mul_2x);
-    std::cout << std::endl << "Unroll_2 time:" << unroll_mul_2x_time.elapsed() << " millisecs\n";
+    OUT << naive_mul_ << "              <================= result\n";
 
 
-    std::cout << "res matrix:\n";
-   // OUT << unroll_mul_2 << "              <================= result\n";
-
-
-    if (naive_mul_ == unroll_mul_2x)
-        std::cout << "Mul is right!\n";
-
-    else
-        std::cout << "Something went wrong!\n";
-
-
-    /* <=====================================================================> */
-
-    Time::Timer unroll_mul_4x_time{};
-
-    mul_unroll_4x(mtr1, mtr2, unroll_mul_4x);
-    std::cout << std::endl << "Unroll_4 time:" << unroll_mul_4x_time.elapsed() << " millisecs\n";
-
-
-    std::cout << "res matrix:\n";
-    // OUT << unroll_mul_2 << "              <================= result\n";
-
-
-    if (naive_mul_ == unroll_mul_4x)
-        std::cout << "Mul is right!\n";
-
-    else
-        std::cout << "Something went wrong!\n";
-
-    /* <=====================================================================> */
-
-    Time::Timer unroll_mul_8x_time{};
-
-    mul_unroll_4x(mtr1, mtr2, unroll_mul_8x);
-    std::cout << std::endl << "Unroll_8 time:" << unroll_mul_8x_time.elapsed() << " millisecs\n";
-
-
-    std::cout << "res matrix:\n";
-    // OUT << unroll_mul_2 << "              <================= result\n";
-
-
-    if (naive_mul_ == unroll_mul_8x)
-        std::cout << "Mul is right!\n";
-
-    else
-        std::cout << "Something went wrong!\n";
-
-    /* <=====================================================================> */
-
-    Time::Timer optimiz_mul{};
-
-    mul_with_intrinsics(mtr1, mtr2, fast_mul);
-    std::cout << std::endl << "Optimazed time:" << optimiz_mul.elapsed() << " millisecs\n";
-
-
-    std::cout << "res matrix:\n";
-    // OUT << fast_mul << "              <================= result\n";
-
-
-    if (naive_mul_ == fast_mul)
-        std::cout << "Mul is right!\n";
-
-    else
-        std::cout << "Something went wrong!\n";
-
+    for (auto&& func: Mul_optim::functions)
+        Mul_testing(func, naive_mul_, lhs, rhs, result);
     
+
     return 0;
+
 }
