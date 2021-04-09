d9b6eb308a0ed4c59f785a6c05fd0173f9eae484
uslsteen <shurygin0171@gmail.com>
2021-04-09 21:10:00 +0300

    correct kernel size to 4x4x4

--- AntonShurygin/2-a/main.cc
+++ AntonShurygin/2-a/main.cc
@@ -57,7 +57,7 @@ void Simple_test_4d()
 void Conv_test()
 {
     uint tens_butch = 4, tens_channels = 8, tens_h = 16, tens_w = 32;
-    uint kernel_butch = 1, kernel_channels = 4, kernel_h = 4, kernel_w = 4; 
+    uint kernel_butch = 16, kernel_channels = 4, kernel_h = 4, kernel_w = 4; 
 
     Tensor_4d tensor{tens_h, tens_w, tens_channels, tens_butch, Mode::RANDOM};
 
