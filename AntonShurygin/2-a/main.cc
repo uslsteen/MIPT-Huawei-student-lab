d90509411a11ba68b01f1778f464cfaab78fb55e
uslsteen <shurygin0171@gmail.com>
2021-04-09 21:03:10 +0300

    correct kernel size to 4x4x4

--- AntonShurygin/2-a/main.cc
+++ AntonShurygin/2-a/main.cc
@@ -57,7 +57,7 @@ void Simple_test_4d()
 void Conv_test()
 {
     uint tens_butch = 4, tens_channels = 8, tens_h = 16, tens_w = 32;
-    uint kernel_butch = 2, kernel_channels = 4, kernel_h = 10, kernel_w = 4; 
+    uint kernel_butch = 1, kernel_channels = 4, kernel_h = 4, kernel_w = 4; 
 
     Tensor_4d tensor{tens_h, tens_w, tens_channels, tens_butch, Mode::RANDOM};
 
@@ -67,15 +67,15 @@ void Conv_test()
 
     Tensor_4d kernel{kernel_h, kernel_w, kernel_channels, kernel_butch, Mode::RANDOM};
 
-    #if 0
+    //#if 0
     std::cout << kernel;
-    #endif
+    //#endif
 
     Time::Timer naive_time;
 
     Tensor_4d result = naive_convolution(tensor, kernel);
 
-    std::cout << "naive_conv time: " << naive_time.elapsed() << " milliseconds";
+    std::cout << "naive_conv time: " << naive_time.elapsed() << " milliseconds\n";
 
     #if 0
     std::cout << result; 
@@ -86,10 +86,5 @@ void Conv_test()
 
 int main()
 {
-    //Simple_test_2d();
-    //Simple_test_3d();
-    //Simple_test_4d();
-
-
     Conv_test();
 }
