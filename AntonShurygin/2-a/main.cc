8dea394358743697b78c35eca9a57ca5755518e0
uslsteen <shurygin0171@gmail.com>
2021-04-09 13:58:06 +0300

    upd

--- AntonShurygin/2-a/main.cc
+++ AntonShurygin/2-a/main.cc
@@ -4,10 +4,11 @@ using namespace Linear_space;
 
 void Simple_test_2d()
 {
-    Tensor_4d tensor{4, 4, 1, 1, 5};
+    std::cout << "simple_testing_2d have started:\n";
+    Tensor_4d tensor{4, 4, 1, 1, 2.5};
     std::cout << tensor;
 
-    Tensor_4d kernel{3, 3, 1, 1, 2};
+    Tensor_4d kernel{3, 3, 1, 1, 4};
     std::cout << kernel;
     
     Tensor_4d result;
@@ -17,10 +18,46 @@ void Simple_test_2d()
     std::cout << result; 
 }
 
+void Simple_test_3d()
+{
+    std::cout << "simple_testing_3d have started:\n";
+    Tensor_4d tensor{9, 9, 3, 1, 1.25};
+    std::cout << tensor;
+
+    Tensor_4d kernel{3, 3, 3, 1, 8};
+    std::cout << kernel;
+    
+    Tensor_4d result;
+
+    result = naive_convolution(tensor, kernel);
+
+    std::cout << result; 
+}
+
+
+void Simple_test_4d()
+{   
+    std::cout << "simple_testing_4d have started:\n";
+
+    Tensor_4d tensor{9, 9, 3, 3, 0.625};
+    std::cout << tensor;
+
+    Tensor_4d kernel{3, 3, 3, 1, 16};
+    std::cout << kernel;
+    
+    Tensor_4d result;
+
+    result = naive_convolution(tensor, kernel);
+
+    std::cout << result; 
+}
+
+
+
+
 int main()
 {
-    
     Simple_test_2d();
-
-    //std::cout << test;
+    Simple_test_3d();
+    Simple_test_4d();
 }
